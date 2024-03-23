89652654347b0d8660718d21998530428594ce14
Simon <simon.ruetz@numbat.energy>
2024-03-23 15:22:16 +0100

    update

--- src/helperfunctions.jl
+++ src/helperfunctions.jl
@@ -44,7 +44,7 @@ function Nesta_Cont(y,z₀,mask,A,At,niter,μ::Float64=0.2, η::Float64=1.,c::Nu
     return z₀
 end
 
-function generate_l2_dist(W,wf,L,N,f,lines)
+function generate_l2_dist(W,wf,L,N,f,lines_and_blocks)
     # W .... 2D Weight matrix of Wavelet Basis
     # wf ... Wavlet filter
     # L .... Maximum Wavelet levels
@@ -53,7 +53,6 @@ function generate_l2_dist(W,wf,L,N,f,lines)
     # Returns the optimal subsampling distribution in the l2 sense
     # 2024 Simon Ruetz
     
-
     # Precompute 1D Wavelet Basis and Fourier transform of wavelet basis 
     Wav_Bas = zeros(L,N,N);
     current = zeros(N);
@@ -65,22 +64,18 @@ function generate_l2_dist(W,wf,L,N,f,lines)
         end
     end
    
-    F_Wav = f(Wav_Bas,2)/sqrt(N)#[:,1:round(Int,N/2+1),:];
-
-
+    F_Wav = f(Wav_Bas,2)/sqrt(N)
     F_Wav_sep = f(Wav_Bas[L,:,:],1)/sqrt(N)
-    @infiltrate
-    #F_Wav = fftshift(F_Wav,2)
+
     # Preallocate memory for speedup. Multithreaded
     Q = [complex(zeros(N,N)) for i = 1:Threads.nthreads()];
     Q_current = [complex(zeros(N,N)) for i = 1:Threads.nthreads()];
-    Q_est = [zeros(N,N) for i = 1:Threads.nthreads()];
+    Q_adapted = [zeros(N,N) for i = 1:Threads.nthreads()];
     current = [zeros(size(W)) for i = 1:Threads.nthreads()];
     n1 = ones(Int64,1,Threads.nthreads());
     n2 = ones(Int64,1,Threads.nthreads());
     lvl = ones(Int64,1,Threads.nthreads());
     K = N^2
-    #U = complex(zeros(K,K))
     p = Progress(sum(sum(W.>=0)), dt=0.5,desc="Generating sampling distribution...",  barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:black);
 
     # Iterate over all entries of the weight matrix W
@@ -93,19 +88,18 @@ function generate_l2_dist(W,wf,L,N,f,lines)
         
         Q_current[i] = F_Wav[ lvl[i] ,:,j[1]]*F_Wav[lvl[i] ,:,j[2]]'; #Take the correct wavelets out of the precomputet matrix
         Q[i] .+= abs.(Q_current[i].^2) .*W[j].^2;
-        Q_est[i] .= max.(Q_est[i] , abs.(Q_current[i].^2)) ;
-        #U[:,(j[2]-1)*N+ j[1]] = reshape(Q_current[i],K,1);
+        Q_adapted[i] .= max.(Q_adapted[i] , abs.(Q_current[i].^2)) ;
         next!(p)
     end  
     Q = abs.(sum(Q)); 
     Q_max = zeros(N,N);
     for i = 1:Threads.nthreads()
-        Q_max = max.(Q_max,Q_est[i])
+        Q_max = max.(Q_max,Q_adapted[i])
     end
     
     K = N^2
-    if lines ==1
-        d = zeros(N)
+    if lines_and_blocks ==1
+        sampling_probabilities_lines = zeros(N)
         
         for ii = 1:N
             U = [complex(zeros(N,N)) for i = 1:Threads.nthreads()];
@@ -123,16 +117,14 @@ function generate_l2_dist(W,wf,L,N,f,lines)
                 next!(p)
             end
             U = sum(U)
-            d[ii] =opnorm(U)
-            println(ii)
-            #println(N)
+            sampling_probabilities_lines[ii] =opnorm(U)
         end
-        d = d/sum(d)
-    #elseif blocks ==1
+
+        sampling_probabilities_lines = sampling_probabilities_lines/sum(sampling_probabilities_lines)
         S2 = Int64(round(sqrt(N)))
         S3 = Int64(round(N/S2))
         
-        dt = zeros(S3,S3)
+        sampling_probabilities_blocks = zeros(S3,S3)
         for ii1 = 1:S3
             for ii2 = 1:S3
                 U = [complex(zeros(S2^2,S2^2)) for i = 1:Threads.nthreads()];
@@ -146,51 +138,37 @@ function generate_l2_dist(W,wf,L,N,f,lines)
                     
                     @views Q_current[i] = reshape(F_Wav_sep[((ii1-1)*S2 + 1) : ii1*S2,j[1]]*transpose(F_Wav_sep[((ii2-1)*S2 + 1) : ii2*S2 ,j[2]]),S2^2,1) * (reshape(F_Wav_sep[((ii1-1)*S2 +1 ): ii1*S2,j[1]]* transpose(F_Wav_sep[((ii2-1)*S2 +1): ii2*S2 ,j[2]]),S2^2,1))' ; #Take the correct wavelets out of the precomputet matrix
                     U[i] .+= Q_current[i].*W[j].^2
-                    #@inline Ut[i][:,(j[2]-1)*N+j[1]] = reshape(Q_current[i]',K,1)
                     next!(p)
                 end
                 U = sum(U)
-                dt[ii1,ii2] =opnorm(U)
-                #dt[ii1,ii2] = maximum(abs.(Q[((ii1-1)*S2 + 1) : ii1*S2,((ii2-1)*S2 + 1) : ii2*S2]))
-                
-                #println(ii2)
-                #println(N)
+                sampling_probabilities_blocks[ii1,ii2] =opnorm(U)
             end
             println(ii1)
         end
 
-       
-        #d = d/sum(d)
-        dt = dt/sum(dt)
+        sampling_probabilities_blocks = sampling_probabilities_blocks/sum(sampling_probabilities_blocks)
     else
-        d = zeros(N)
-        dt = zeros(N)
-        U = 0
+        sampling_probabilities_lines = zeros(N)
+        sampling_probabilities_blocks = zeros(N)
     end
 
     # Combine the Threads back
-    
     Q_max = abs.(Q_max);
-    Q_est = max.(Q,Q_max)
+    Q_adapted = max.(Q,Q_max)
    
-    Q = Q./sum(sum(Q));
     Q_max = Q_max./sum(sum(Q_max));
-    Q_est = Q_est./sum(Q_est)
+    Q_adapted = Q_adapted./sum(Q_adapted)
 
-    Dist1 = zeros(N,N);
-    Dist1 = Q_est;
-    Dist1 = Dist1./sum(sum(Dist1));
-    
-    Dist2 = zeros(N,N);
-    Dist2 = Q;
-    Dist2 = Dist2./sum(sum(Dist2));    
+    adapted_distribution = zeros(N,N);
+    adapted_distribution = Q_adapted;
+    adapted_distribution = adapted_distribution./sum(sum(adapted_distribution));
 
-    Dist3 = zeros(N,N);
-    Dist3 = Q_max;
-    Dist3 = Dist3./sum(sum(Dist3));    
+    max_coherence_distribution = zeros(N,N);
+    max_coherence_distribution = Q_max;
+    max_coherence_distribution = max_coherence_distribution./sum(sum(max_coherence_distribution));    
 
 
-    return Dist1, Dist2, Dist3 ,U ,d , dt
+    return adapted_distribution, max_coherence_distribution, sampling_probabilities_lines, sampling_probabilities_blocks
 end
 
 function process_image(path_vec::String,wf,L,N::Int64,th,flp,dwts)
@@ -223,7 +201,6 @@ function process_image(path_vec::String,wf,L,N::Int64,th,flp,dwts)
                     else
                         C[i] = dwts(img[i],wf,L);
                     end
-                    #print(size(C[i]))
                     coeffs[i] +=  (abs.(C[i]) .>= th*norm(C[i]));#sort(vec(abs.(C[i])))[end-165];#
                 end
                 next!(p)
@@ -239,7 +216,6 @@ function process_image(path_vec::String,wf,L,N::Int64,th,flp,dwts)
             else
                 C[i] = dwts(img[i],wf,L);
             end
-            #print(size(C[i]))
             coeffs[i] += abs.(C[i]) .>= th*norm(C[i]);#sort(vec(abs.(C[i])))[end-165];#
             next!(p)
     end
@@ -252,9 +228,81 @@ function process_image(path_vec::String,wf,L,N::Int64,th,flp,dwts)
     S_av = round(sum(sum(coeff)/Nmax))
     W = sqrt.(coeff./Nmax);
     W = coeff./Nmax;
-    #@infiltrate
     return coeff, Nmax, S_av, W
 end
 
 
+function calculate_norm(W,wf,L,N,f,lines_and_blocks,ind)
+    # W .... 2D Weight matrix of Wavelet Basis
+    # wf ... Wavlet filter
+    # L .... Maximum Wavelet levels
+    # N .... Dimension
+    # 
+    # Returns the optimal subsampling distribution in the l2 sense
+    # 2022 Simon Ruetz
+    
 
+    # Precompute 1D Wavelet Basis and Fourier transform of wavelet basis 
+    Wav_Bas = zeros(L,N,N);
+    current = zeros(N);
+    for j = 1:L
+        for i = 1:N
+            current[i] = 1;
+            Wav_Bas[j,:,i] =idwt(current,wf,j);
+            current[i] = 0;
+        end
+    end
+    
+    F_Wav = f(Wav_Bas,2)/sqrt(N)#[:,1:round(Int,N/2+1),:];
+    #F_Wav = fftshift(F_Wav,2)
+    # Preallocate memory for speedup. Multithreaded
+    Q = [complex(zeros(N,N)) for i = 1:Threads.nthreads()];
+    Q_current = [complex(zeros(N,N)) for i = 1:Threads.nthreads()];
+    Q_est = [zeros(N,N) for i = 1:Threads.nthreads()];
+    current = [zeros(size(W)) for i = 1:Threads.nthreads()];
+    n1 = ones(Int64,1,Threads.nthreads());
+    n2 = ones(Int64,1,Threads.nthreads());
+    lvl = ones(Int64,1,Threads.nthreads());
+    K = N^2
+    #U = complex(zeros(K,K))
+    p = Progress(sum(sum(W.>=0)), dt=0.5,desc="Calculating norm...",  barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:black);
+
+    K = N^2
+    S2 = Int64(round(1))
+    S3 = Int64(round(N^2))
+
+    dt = zeros(length(ind),S3)
+    iii = 1
+    
+    for ii1 = 1:N
+        for ii2 = 1:N
+            a = (ii1-1)*N + ii2
+            if issubset(a,ind)
+                U = [complex(zeros(length(ind),S3)) for i = 1:Threads.nthreads()];
+                Q_current = [complex(zeros(1,1)) for i = 1:Threads.nthreads()];
+                Threads.@threads for j in findall(W.>0)
+                    i = Threads.threadid();
+                    # calculate levels of different wavelets for j_1,j_2
+                    n1[i] = Int(L-ceil(log2(j[1]))+1);
+                    n2[i] = Int(L-ceil(log2(j[2]))+1);
+                    lvl[i] = min(n1[i],n2[i],L);
+                    
+                    #@views Q_current[i] =  ; #Take the correct wavelets out of the precomputet matrix
+                    @inline U[i][iii,(j[2]-1)*N+j[1]] = F_Wav[ lvl[i],ii1,j[1]]*adjoint(F_Wav[lvl[i],ii2,j[2]]).*W[j]
+                    #@inline Ut[i][:,(j[2]-1)*N+j[1]] = reshape(Q_current[i]',K,1)
+                    next!(p)
+                end
+                U = sum(U)
+                dt += U
+                #dt[ii1,ii2] = maximum(abs.(Q[((ii1-1)*S2 + 1) : ii1*S2,((ii2-1)*S2 + 1) : ii2*S2]))
+                iii += 1
+                
+                #println(N)
+            end
+
+        end
+        
+    end
+    println(iii)
+    return opnorm(dt)
+end
--- src/main.jl
+++ src/main.jl
@@ -1,20 +1,20 @@
 
 function wavelet_test()
-    run_tests(; K=2^16, lk=8.0, flp=0, had=0, lines=0, blocks=0,
+    run_tests(; K=2^16, lk=8.0, flp=0, had=0, lines_and_blocks=0,
         train_path=joinpath(pwd(), "images", "Brain4"),
         test_path=joinpath(pwd(), "images", "brain4.tif"),
         wf=wavelet(WT.db4, WT.Filter), R=10, sep=0)
 end
 
 function wavelet_test_2()
-    run_tests(; K=2^16, lk=8.0, flp=0, had=0, lines=0, blocks=0,
+    run_tests(; K=2^16, lk=8.0, flp=0, had=0, lines_and_blocks=0,
         train_path=joinpath(pwd(), "images", "Brain4"),
         test_path=joinpath(pwd(), "images", "brain4.tif"),
         wf=wavelet(WT.db4, WT.Filter), R=10, sep=0)
 end
 
 function levels_test()
-    run_tests(; K=2^18, lk=9.0, flp=0, had=0, lines=0, blocks=0,
+    run_tests(; K=2^18, lk=9.0, flp=0, had=0, lines_and_blocks=0,
         train_path=joinpath(pwd(), "images", "Brain5"),
         test_path=joinpath(pwd(), "images", "brain.png"),
         wf=wavelet(WT.db4, WT.Filter), R=10, sep=0)
@@ -22,14 +22,14 @@ end
 
 
 function flip_test()
-    run_tests(; K=2^16, lk=15.0, flp=1, had=0, lines=0, blocks=0,
+    run_tests(; K=2^16, lk=15.0, flp=1, had=0, lines_and_blocks=0,
         train_path=joinpath(pwd(), "images", "Brain4"),
         test_path=joinpath(pwd(), "images", "brain.png"),
         wf=wavelet(WT.db4, WT.Filter), R=20, sep=0)
 end
 
 function knee_test()
-    run_tests(; K=2^16, lk=7.0, flp=0, had=0, lines=0, blocks=0,
+    run_tests(; K=2^16, lk=7.0, flp=0, had=0, lines_and_blocks=0,
         train_path=joinpath(pwd(), "images", "MRNet", "train", "coronal"),
         test_path=joinpath(pwd(), "images", "MRNet", "valid", "coronal", "1190.npy"),
         wf=wavelet(WT.db4, WT.Filter), R=10, sep=0)
@@ -40,7 +40,7 @@ function knee_test()
 end
 
 function line_test()
-    run_tests(; K=2^16, lk=59.0, flp=0, had=0, lines=1, blocks=1,
+    run_tests(; K=2^16, lk=59.0, flp=0, had=0, lines_and_blocks=1,
         train_path=joinpath(pwd(), "images", "Brain4"),
         test_path=joinpath(pwd(), "images", "brain4.tif"),
         wf=wavelet(WT.db4, WT.Filter), R=5, sep=1)
@@ -51,7 +51,7 @@ function run_tests(;
     #DB4 : Flip and Knee
     #K::Int64 = 2^16, lk::Float64 = 19.,flp::Int64 = 0, had::Int64 = 0, lines::Int64 = 0,
     #train_path::String = joinpath(pwd(), "images", "MRNet","train","coronal") ,test_path::String = joinpath(pwd(),"images", "MRNet","valid","coronal","1190.npy"))
-    K::Int64=2^18, lk::Float64=0.0, flp::Int64=0, had::Int64=0, lines::Int64=0, blocks::Int64=0,
+    K::Int64=2^18, lk::Float64=0.0, flp::Int64=0, had::Int64=0, lines_and_blocks::Int64=0,
     train_path::String=joinpath(pwd(), "images", "Brain4"), test_path::String=joinpath(pwd(), "images", "brain.png"),
     wf=wavelet(WT.db4, WT.Filter), R=20, sep=1)
 
@@ -64,8 +64,6 @@ function run_tests(;
     function wav(x)
         dwt(x, wf, L)
     end
-
-
 
     function iwav(x)
         idwt(x, wf, L)
@@ -98,16 +96,14 @@ function run_tests(;
         ft = ifft
     end
 
-
     A(x) = f(idwts(x, wf, L)) / N
     At(x) = dwts(ft(x) * N, wf, L)
+
     # Load, transform and threshold images form path to get proxy of wavelet distribution W
     coeff, Nmax, S_av, W = process_image(train_path, wf, L, N, th, flp, dwts)
 
+    Dist1, Dist3 , sampling_probabilities_lines, sampling_probabilities_blocks = generate_l2_dist(W, wf, L, N, f, lines_and_blocks)
 
-    Dist1, Dist2, Dist3, U, d, dt = generate_l2_dist(W, wf, L, N, f, lines)
-    #Dist1 = max.(Dist1,Dist2)
-    #@infiltrate
     Dist2 = zeros(N, N)
     if had == 0
         for i = 1:N
@@ -124,53 +120,31 @@ function run_tests(;
         end
     end
     Dist2 = Dist2 / sum(Dist2)
-    #Dist2 = ones(N,N)/K;
-    d1 = d#[fftshift(sum(fftshift(Dist1),dims = 2)) fftshift(sum(fftshift(Dist1), dims =1))'];
 
-    d2 = dt
-    #[fftshift(sum(fftshift(Dist2),dims = 2)) fftshift(sum(fftshift(Dist2), dims =1))'];
-    #d1, d3 = gen_lines(W,wf,L,N,f)
+
+    # now sample from the distributions
     mask1 = zeros(N, N)
     mask2 = zeros(N, N)
 
-
-    if lines == 1
-        # for i = 1:N
-        #     d2[i] = 1/max(1,(i-N/2)^2)^(2.5);
-        # end
-        # d2 = fftshift(d2)/sum(d2)
-        w1 = FrequencyWeights(vec(d1[1:N]))
+    if lines_and_blocks == 1
+        # lines:
+        w1 = FrequencyWeights(vec(sampling_probabilities_lines[1:N]))
         ind1 = sample(1:N, w1, Int(round(N / R)); replace=false)
         mask1[ind1[ind1.<=N], :] .= 1
         mask1[:, ind1[ind1.>=N+1].-N] .= 1
-        w2 = FrequencyWeights(vec(d2))
 
-        ind2 = sample(1:length(d2), w2, Int(round(N / R)); replace=false)
-        #ind22 = collect(Int64(round(ind2[1]*sqrt(N))):(Int64(round(ind2[2]*sqrt(N)+sqrt(N)-1))))';
-
-
-        for l in eachindex(d2)
+        # blocks:
+        w2 = FrequencyWeights(vec(sampling_probabilities_blocks))
+        ind2 = sample(1:length(sampling_probabilities_blocks), w2, Int(round(N / R)); replace=false)
+        for l in eachindex(sampling_probabilities_blocks)
             In = Int64(round(sqrt(N)))
             ll1 = mod(l - 1, Int64(round(N / In)))
             ll2 = (l - 1) ÷ Int64(round(N / In))
             if l in ind2
                 mask2[ll1*In+1:Int64(round(ll1 * In + In)), ll2*In+1:Int64(round(ll2 * In + In))] .= 1
             end
-            Dist2[ll1*In+1:Int64(round(ll1 * In + In)), ll2*In+1:Int64(round(ll2 * In + In))] .= d2[l]
+            Dist2[ll1*In+1:Int64(round(ll1 * In + In)), ll2*In+1:Int64(round(ll2 * In + In))] .= sampling_probabilities_blocks[l]
         end
-
-        #mask2[:,ind2[ind2.>=N+1].-N] .= 1;
-        # elseif lines == 2
-        #     # d1, d2 = gen_lines(W,wf,L,N,f)
-        #     # d2 = [fftshift(sum(fftshift(Dist2),dims = 1)) fftshift(sum(fftshift(Dist2), dims =2))];
-        #     w1 = FrequencyWeights(vec(d1[1:2*N]));
-        #     ind1 = sample(1:2*N,w1,Int(round(N/R));replace=false);
-        #     mask1[ind1[ind1.<=N],:].= 1;
-        #     mask1[:,ind1[ind1.>=N+1].-N] .= 1;
-        #     w2 = FrequencyWeights(d2[]);
-        #     ind2 = sample(1:2*N,w2,Int(round(N/R));replace=false);
-        #     mask2[ind2[ind2.<=N],:].= 1;
-        #     mask2[:,ind2[ind2.>=N+1].-N] .= 1;
     else
         w1 = FrequencyWeights(vec(reshape(Dist1, 1, K)))
         ind1 = sample(1:K, w1, Int(round(K / R)); replace=false)
@@ -180,7 +154,7 @@ function run_tests(;
         mask2[ind2] .= 1
     end
 
-
+    # load the image on which we test the performance of the sampling algorithms
     Im = load(test_path)
     if length(size(Im)) > 2
         l = Int64(round(size(Im)[1] * 1 / 10))
@@ -194,31 +168,23 @@ function run_tests(;
         Im = flip(Im, wf, L, K, N)
     end
     Im = Im ./ norm(Im)
-    #mask1 = mask1'
-    #W[W .== 0 ] .= 0.1^10
 
-
-    #@sync begin
+    # l1 minimisation starts here:
     δ = 1.0e-7
     μ = 0.2
-    #@async begin
+
     #y = mask1.*f(idwt(pinv.(W).*dwt(Im,wf,L),wf,L))/N;
     y = mask1 .* f(Im) / N
     im_rec = Nesta_Cont(y, y, mask1, A, At, iter, μ, δ, 1.0, 10)
     #im_rec = idwt(W.*dwt(ft(im_rec),wf,L),wf,L)
     im_rec = ft(im_rec) * N
-    #im_rec = (im_rec .- minimum(im_rec))./(maximum(im_rec) - minimum(im_rec));
     im_rec = im_rec / norm(im_rec)
-    #end
 
-    #@async begin
+
     y = mask2 .* f(Im) / N
     im_rec2 = Nesta_Cont(y, y, mask2, A, At, iter, μ, δ, 1.0, 10)
     im_rec2 = ft(im_rec2) * N
-    #im_rec = (im_rec .- minimum(im_rec))./(maximum(im_rec) - minimum(im_rec));
     im_rec2 = im_rec2 / norm(im_rec2)
-    #end
-    #end
     if flp == 1
         im_rec = flip(im_rec, wf, L, K, N)
         im_rec2 = flip(im_rec2, wf, L, K, N)
@@ -227,41 +193,36 @@ function run_tests(;
 
     end
 
-    #@infiltrate
-    #im_rec = idwt(fliptest(dwt(im_rec,wf,L)),wf,L);
-    #im_rec2 = idwt(fliptest(dwt(im_rec2,wf,L)),wf,L);
-    #var_[1,2,lk] = psnr(abs.(idwt(rec1,wf,L)),Im_org,maximum(abs.(Im_org))); 
     var1 = psnr(real.(im_rec), Im, maximum(abs.(Im)))
     var2 = psnr(real.(im_rec2), Im, maximum(abs.(Im)))
 
-
     Dist1[Dist1.<=exp(-60)] .= exp(-60)
-    #
-    if had == 0
-        clims = (minimum([Dist1 Dist2]), maximum([Dist1 Dist2]))
-        fig = Figure(resolution = (1800, 1000))
-        #ax, hm = contourf(fig[1, 1], fftshift(log.(Dist1')), levels=0:-1:-27, colorrange=log.(clims))
-        ax, hm = contourf(fig[1, 1], fftshift(log.(Dist1')), colorrange=log.(clims))
+
+    if lines_and_blocks == 1
+        @infiltrate
+        fig = Figure(resolution = (2000, 1800))
+        CairoMakie.Axis(fig[1, 1])
+        lines!(1:N, fftshift(log.(sampling_probabilities_lines)))
+        #CairoMakie.Axis(fig[1,2])
+        ax, hm = heatmap(fig[1, 2], fftshift(log.(Dist2)))
         ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
         ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        Colorbar(fig[1, 2], hm)
-        #ax, hm = contourf(fig[1, 3], fftshift(log.(Dist2')), levels=0:-1:-27, colorrange=log.(clims))
-        ax, hm = contourf(fig[1, 3], fftshift(log.(Dist2')), colorrange=log.(clims))
-        ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        Colorbar(fig[1, 4], hm)
+        Colorbar(fig[1, 3], hm)
         ax, hm = heatmap(fig[2, 1], fftshift(mask1)', colormap=:grays)
         ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
         ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax, hm = heatmap(fig[2, 3], fftshift(mask2)', colormap=:grays)
+        ax, hm = heatmap(fig[2, 2], fftshift(mask2)', colormap=:grays)
         ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
         ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        #ax, hm = heatmap(fig[1,5],fftshift(log.(abs.(fft(Im))))',colormap = :grays)
-        ax, hm = heatmap(fig[1, 5], W' .^ 2, colormap=:grays)
-        Colorbar(fig[1, 6], hm)
-        ax, hm = heatmap(fig[2, 5], abs.(Im)', colormap=:grays)
+        ax, hm = heatmap(fig[1, 4], W', colormap=:grays)
+        Colorbar(fig[1, 5], hm)
+        #ax, hm = heatmap(fig[2,3],abs.(Im)',colormap = :grays)
+        ax, hm = heatmap(fig[3, 1], abs.(im_rec)', colormap=:grays)
+        ax, hm = heatmap(fig[3, 2], abs.(im_rec2)', colormap=:grays)
+        ax, hm = heatmap(fig[3, 4], abs.(Im)', colormap=:grays)
         display(fig)
-    else
+        save("lines.pdf", fig)
+    else 
         clims = (minimum([Dist1 Dist2]), maximum([Dist1 Dist2]))
         fig = Figure(resolution = (1800, 1000))
         ax, hm = contourf(fig[1, 1], log.(Dist1'),  colorrange=log.(clims))
@@ -283,46 +244,11 @@ function run_tests(;
         ax, hm = heatmap(fig[2, 5], abs.(Im)', colormap=:grays)
         display(fig)
     end
-
-    if lines == 1
-        @infiltrate
-        fig = Figure(resolution = (2000, 1800))
-        CairoMakie.Axis(fig[1, 1])
-        lines!(1:N, fftshift(log.(d1)))
-        #CairoMakie.Axis(fig[1,2])
-        ax, hm = heatmap(fig[1, 2], fftshift(log.(Dist2)))
-        ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        Colorbar(fig[1, 3], hm)
-        ax, hm = heatmap(fig[2, 1], fftshift(mask1)', colormap=:grays)
-        ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax, hm = heatmap(fig[2, 2], fftshift(mask2)', colormap=:grays)
-        ax.xticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax.yticks = (1:64:N, string.(Int.(-N/2:64:N/2-1)))
-        ax, hm = heatmap(fig[1, 4], W', colormap=:grays)
-        Colorbar(fig[1, 5], hm)
-        #ax, hm = heatmap(fig[2,3],abs.(Im)',colormap = :grays)
-        ax, hm = heatmap(fig[3, 1], abs.(im_rec)', colormap=:grays)
-        ax, hm = heatmap(fig[3, 2], abs.(im_rec2)', colormap=:grays)
-        ax, hm = heatmap(fig[3, 4], abs.(Im)', colormap=:grays)
-        display(fig)
-        save("lines.pdf", fig)
-    end
-
-    #
-
-
-    #println(sum(sum(mask1))/K)
+    println("adapted variable density sub psnr:")
     println(var1)
+    println("Comparison:")
     println(var2)
 
-
-    #println(sum(mask2[:,Int(round(N/2))]))
-    #println(sum(mask2[Int(round(N/2)),:]))
-
-    println("S is:")
-    println(S_av)
     return var1, var2
 end
 
